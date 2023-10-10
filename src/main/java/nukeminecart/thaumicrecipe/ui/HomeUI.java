5a55055c2146f5d7bbf7e26ab175f4389343f661
NukeMinecart <bradley.chumanov@gmail.com>
2023-10-10 12:10:21 -0500

    Begin creating the ui for the home screen

--- src/main/java/nukeminecart/thaumicrecipe/ui/HomeUI.java
+++ src/main/java/nukeminecart/thaumicrecipe/ui/HomeUI.java
@@ -2,8 +2,16 @@ package nukeminecart.thaumicrecipe.ui;
 
 import javax.swing.*;
 
-public class HomeUI {
+public class HomeUI extends JFrame{
     private JPanel home;
     private JButton createButton;
     private JButton loadButton;
+    private JTextField createConfig;
+    private JTextField loadConfig;
+
+    public HomeUI(){
+        setTitle("Home");
+        setContentPane(home);
+        setVisible(true);
+    }
 }

98fe705aed156ff79ae9585b63299a4cb2d85283
NukeMinecart <bradley.chumanov@gmail.com>
2023-11-17 12:15:01 -0600

    Add research stuff to remember

--- src/main/java/nukeminecart/thaumicrecipe/ThaumicRecipeTweaker.java
+++ src/main/java/nukeminecart/thaumicrecipe/ThaumicRecipeTweaker.java
@@ -4,8 +4,14 @@ import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import thaumcraft.api.research.ResearchCategories;
+import thaumcraft.api.research.ResearchCategory;
+import thaumcraft.api.research.ResearchEntry;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Mod(modid = ThaumicRecipeTweaker.MODID, useMetadata = true)
 public class ThaumicRecipeTweaker
@@ -20,6 +26,16 @@ public class ThaumicRecipeTweaker
             URL directoryURL = DirectoryLocator.getLocation(ThaumicRecipeTweaker.class);
             String directory = DirectoryLocator.urlToFile(directoryURL).getPath().replace("ThaumicRecipeTweaker-" + VERSION + ".jar", "");
             new JarExecutor().executeJar((directory + ThaumicRecipeTweaker.GUIID + ".jar"), Minecraft.getMinecraft().mcDataDir.getPath(), Config.loadedRecipeFile);
+        }
+        List<Map<String, ResearchEntry>> researchEntries = new ArrayList<>();
+        List<String> researchKeys = new ArrayList<>();
+        for(ResearchCategory category: ResearchCategories.researchCategories.values()){
+            researchEntries.add(category.research);
+        }
+        for(Map<String, ResearchEntry> entry : researchEntries){
+            for(ResearchEntry researchEntry : entry.values()){
+                researchKeys.add(researchEntry.getKey());
+            }
         }
     }
 }
